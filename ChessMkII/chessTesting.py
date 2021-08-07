aee6ae7d313452ba0b60a0dff1752fcadbd69e01
Ayden Hogeveen <a.hogeveen@share.epsb.ca>
2021-08-06 21:31:13 -0600

    played around with some piece movement, don't have it yet

--- ChessMkII/chessTesting.py
+++ ChessMkII/chessTesting.py
@@ -6,6 +6,7 @@ date-created: 2021-07-27
 """
 
 import pygame
+from chess_engine import *
 
 pygame.init()
 
@@ -160,6 +161,16 @@ class DragDrop:
     def __init__(self):
         self.is_held = False
 
+    def holding_piece(self, rect, pos):
+        rect_x, rect_y, rect_width, rect_height = rect
+
+        player_x, player_y = pos
+
+        if rect_y <= player_y <= rect_y + rect_height and rect_x <= player_x <= rect_x + rect_width:
+            return True
+        else:
+            return False
+
 
 # Main Class
 class Main:
@@ -172,14 +183,24 @@ class Main:
         self.running = True
         self.board = Board()
         self.drag_drop = DragDrop()
+        self.imgs = self.board.images
+        self.square_selected = ()
+        self.player_clicks = []
         self.clock = pygame.time.Clock()
 
     def run(self):
-
+        global piece
         while self.running:
             # Setting the background colour
             screen.fill(Colour.BLACK)
             self.board.drawGame(self.board.virtual_board)
+
+            pieces = ["pawn_white", "rook_white", "knight_white", "bishop_white", "queen_white", "king_white",
+                      "pawn_black", "rook_black", "knight_black", "bishop_black", "queen_black", "king_black"]
+            for piece in pieces:
+                self.imgs[piece] = pygame.transform.scale(
+                    pygame.image.load("chessAssets/ChessPieces/" + piece + ".png"),
+                    (self.board.square_size, self.board.square_size))
 
             # Tracking the mouse's position
             mouse_pos = pygame.mouse.get_pos()
@@ -205,6 +226,12 @@ class Main:
                     self.drag_drop.is_held = False
                     if not self.drag_drop.is_held:
                         print("dropped at", mouse_rank, mouse_file)
+
+                if event.type == pygame.MOUSEMOTION:
+                    # Holding the piece while moving the mouse
+                    if self.drag_drop.is_held:
+                        piece_width = self.imgs[piece].get_width()
+                        piece_height = self.imgs[piece].get_height()
 
             # Updates the Screen
             pygame.display.update()
---
+++ ChessMkII/chess_engine.py
@@ -0,0 +1,6 @@
+# chess_engine.py
+
+class game_state:
+    def __init__(self):
+        pass
+
