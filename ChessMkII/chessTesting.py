522baa1b2b374066d7d2304815070240c5e4b9a2
Ayden Hogeveen <a.hogeveen@share.epsb.ca>
2021-08-10 22:02:19 -0600

    trying to work on visual piece movement, not too much to show for it at the moment, other than a method that prints the translated positions of each click

--- ChessMkII/chessTesting.py
+++ ChessMkII/chessTesting.py
@@ -54,23 +54,6 @@ class Colour:
     DARK_GREEN = (0, 50, 0)
 
 
-# Engine Class
-class Engine:
-    def __init__(self):
-        self.white_to_move = True
-        self.black_to_move = True
-        self.is_mate = False
-        self.is_stalemate = False
-        self.is_pawn_promotion = False
-        self.is_en_passant = False
-
-    def move(self):
-        pass
-
-    def findLegalMoves(self, piece_type):
-        pass
-
-
 # Piece Master Class
 class Piece:
     """
@@ -165,6 +148,12 @@ class Board:
         self.loadImages()
         self.drawBoard()
         self.drawPieces(virtual_board)
+        pieces = ["pawn_white", "rook_white", "knight_white", "bishop_white", "queen_white", "king_white",
+                  "pawn_black", "rook_black", "knight_black", "bishop_black", "queen_black", "king_black"]
+        for piece in pieces:
+            self.images[piece] = pygame.transform.scale(
+                pygame.image.load("chessAssets/ChessPieces/" + piece + ".png"),
+                (self.square_size, self.square_size))
 
 
 # Drag and Drop Class
@@ -183,6 +172,62 @@ class DragDrop:
             return False
 
 
+# Engine Class
+class Engine:
+    def __init__(self):
+        self.board = Board()
+        self.white_to_move = True
+        self.black_to_move = True
+        self.is_mate = False
+        self.is_stalemate = False
+        self.is_pawn_promotion = False
+        self.is_en_passant = False
+        self.move_log = []
+
+    def move(self, move):
+        self.board.virtual_board[move.start_rank][move.start_file] = "0"
+        self.board.virtual_board[move.end_rank][move.end_file] = move.piece_moved
+        self.move_log.append(move)
+        if self.white_to_move:
+            self.white_to_move = False
+        else:
+            self.white_to_move = True
+
+    def findLegalMoves(self, piece_type):
+        pass
+
+
+# Move Class
+class Move:
+    def __init__(self, start_square, end_square, virtual_board):
+        self.start_rank = start_square[0]
+        self.start_file = start_square[1]
+        self.end_rank = end_square[0]
+        self.end_file = end_square[1]
+        self.piece_moved = virtual_board[self.start_rank][self.start_file]
+        self.piece_captured = virtual_board[self.end_rank][self.end_file]
+        self.move_id = self.start_rank * 10 + self.start_file * 1 + self.end_rank * 0.1 + self.end_file * 0.01
+
+        self.translate_ranks = {"1": 7, "2": 6, "3": 5, "4": 4, "5": 3, "6": 2, "7": 1, "8": 0}
+        self.translated_ranks = {translate: key for key, translate in self.translate_ranks.items()}  # Reverses a Dictionary
+        self.translate_files = {"a": 0, "b": 1, "c": 2, "d": 3, "e": 4, "f": 5, "g": 6, "h": 7}
+        self.translated_files = {translate: key for key, translate in self.translate_files.items()}  # Reverses a Dictionary
+
+    def __eq__(self, other):
+        """
+        Overriding the equals method
+        """
+        if isinstance(other, Move):
+            return self.move_id == other.move_id
+        return False
+
+    def getRankFile(self, rank, file):
+        return self.translated_files[file] + self.translated_ranks[rank]
+
+    def getChessNotation(self):
+        return self.getRankFile(self.start_file, self.start_rank) + self.getRankFile(self.end_file, self.end_rank)
+
+
 # Main Class
 class Main:
     """
@@ -193,31 +238,28 @@ class Main:
     def __init__(self):
         self.running = True
         self.board = Board()
+        self.engine = Engine()
         self.drag_drop = DragDrop()
         self.imgs = self.board.images
         self.square_selected = ()
-        self.player_clicks = []
+        self.player_clicked = []
         self.clock = pygame.time.Clock()
 
     def run(self):
         global piece
+
+        move_made = False  # Flag for moves that are made
+
         while self.running:
             # Setting the background colour
             screen.fill(Colour.BLACK)
             self.board.drawGame(self.board.virtual_board)
 
-            pieces = ["pawn_white", "rook_white", "knight_white", "bishop_white", "queen_white", "king_white",
-                      "pawn_black", "rook_black", "knight_black", "bishop_black", "queen_black", "king_black"]
-            for piece in pieces:
-                self.imgs[piece] = pygame.transform.scale(
-                    pygame.image.load("chessAssets/ChessPieces/" + piece + ".png"),
-                    (self.board.square_size, self.board.square_size))
-
             # Tracking the mouse's position
             mouse_pos = pygame.mouse.get_pos()
 
-            mouse_rank = mouse_pos[0]//self.board.square_size
-            mouse_file = mouse_pos[1]//self.board.square_size
+            mouse_rank = mouse_pos[0] // self.board.square_size  # Refers to the X position of the mouse, in terms of squares
+            mouse_file = mouse_pos[1] // self.board.square_size  # Refers to the Y position of the mouse, in terms of squares
 
             # Tracking events
             for event in pygame.event.get():
@@ -230,19 +272,40 @@ class Main:
                     # Make the state 'held'
                     self.drag_drop.is_held = True
                     if self.drag_drop.is_held:
-                        print("Holding a", self.board.virtual_board[mouse_file][mouse_rank], "at", mouse_rank, mouse_file)
+                        held_piece = self.board.virtual_board[mouse_file][mouse_rank]
+                        print("Holding a", held_piece, "at", mouse_rank, mouse_file)
 
-                if event.type == pygame.MOUSEBUTTONUP:
-                    # Make the state 'dropped'
-                    self.drag_drop.is_held = False
-                    if not self.drag_drop.is_held:
-                        print("dropped at", mouse_rank, mouse_file)
+                    if self.square_selected == (mouse_rank, mouse_file):  # The player clicks the same square again
+                        # Reset, deselecting the piece
+                        self.square_selected = ()
+                        self.player_clicked = []
+                    else:  # Player clicks a different square
+                        self.square_selected = (mouse_rank, mouse_file)
+                        self.player_clicked.append(self.square_selected)
 
-                if event.type == pygame.MOUSEMOTION:
-                    # Holding the piece while moving the mouse
-                    if self.drag_drop.is_held:
-                        piece_width = self.imgs[piece].get_width()
-                        piece_height = self.imgs[piece].get_height()
+                    if len(self.player_clicked) == 2:  # The player has clicked 2 different squares
+                        # Move the piece on the first square to the second square
+                        move = Move(self.player_clicked[0], self.player_clicked[1], self.board.virtual_board)
+                        print(move.getChessNotation())
+
+                        self.engine.move(move)
+                        print(self.board.virtual_board)
+                        move_made = True
+                        self.square_selected = ()
+                        self.player_clicked = []
+
+                # if event.type == pygame.MOUSEBUTTONUP:
+                #     # Make the state 'dropped'
+                #     self.drag_drop.is_held = False
+                #     if not self.drag_drop.is_held:
+                #         pass
+                #         print("dropped at", mouse_rank, mouse_file)
+
+                # if event.type == pygame.MOUSEMOTION:
+                #     # Holding the piece while moving the mouse
+                #     if self.drag_drop.is_held:
+                #         piece_width = self.imgs[piece].get_width()
+                #         piece_height = self.imgs[piece].get_height()
 
             # Updates the Screen
             pygame.display.update()
